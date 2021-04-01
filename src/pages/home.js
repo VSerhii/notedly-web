06c823fcda2928c673e9de11f871741702f9e248
VSerhii <serhiyvyazelenko@yahoo.com>
2021-04-01 14:34:09 +0200

    initial commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-
 import NoteFeed from '../components/NoteFeed';
 import Button from '../components/Button';
 import { GET_NOTES } from '../gql/query';
 
 const Home = () => {
 	// query hook
-	const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
-
+	const { loading, error, data, fetchMore } = useQuery(GET_NOTES);
 	// if the data is loading, display a loading message
 	if (loading) return <p>Loading...</p>;
 	// if there is an error fetching the data, display an error message
 	if (error) return <p>Error!</p>;
-
 	// if the data is successful, display the data in our UI
 	return (
 		<React.Fragment>
@@ -23,7 +20,7 @@ const Home = () => {
 					onClick={() =>
 						fetchMore({
 							variables: {
-								cursor: data.noteFeed.cursor
+								cursor: data.noteFeed.cursor,
 							},
 							updateQuery: (previousResult, { fetchMoreResult }) => {
 								return {
